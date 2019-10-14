39316560f51e511aff01962eea7fabcc14a62a2c
Jonas Johansson <jns.johansson@gmail.com>
2019-10-14 23:48:30 +0200

    Update app.js

--- _includes/assets/js/app.js
+++ _includes/assets/js/app.js
@@ -12,27 +12,6 @@ window.onload = function() {
 	}
 };
 
-const easeInCubic = function(t) {
-	return t * t * t;
-};
-
-const scrollToElem = (startTime, currentTime, duration, scrollEndElemTop, startScrollOffset) => {
-	const runtime = currentTime - startTime;
-	let progress = runtime / duration;
-
-	progress = Math.min(progress, 1);
-
-	const ease = easeInCubic(progress);
-
-	window.scroll(0, startScrollOffset + scrollEndElemTop * ease);
-	if (runtime < duration) {
-		requestAnimationFrame(timestamp => {
-			const currentTime = timestamp || new Date().getTime();
-			scrollToElem(startTime, currentTime, duration, scrollEndElemTop, startScrollOffset);
-		});
-	}
-};
-
 window.onscroll = function() {
 	myFunction();
 };
